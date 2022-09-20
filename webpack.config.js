81e6b8b14dce17e988934d15c79d1b9046907f7c
nicole-russack <russack@usc.edu>
2022-09-19 17:36:17 -0700

    VTI file viewing works

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,10 @@
-const webpack = require('webpack');
-//const vtkRules = require('vtk.js/Utilities/config/dependency.js').webpack.core.rules
-
 module.exports = {
-    entry: [ './src/index.js' ],
     module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-          },
-          {
-            test: /\.svg$/,
-            use: ['@svgr/webpack'],
-          },
-          {
-            test: /\.(glsl|vs|fs|vert|frag)$/,
-            exclude: /node_modules/,
-            use: [
-              'raw-loader',
-              'glslify-loader'
-            ]
-          },
-          
-          
-          {
-            test: /\.(glsl|frag|vert)$/,
-            use: ['glslify-import-loader', 'raw-loader', 'glslify-loader']
-        },
+      rules: [
         {
-            test: /\.(glsl|frag|vert)$/,
-            exclude: /node_modules/,
-            loader: 'glslify-import-loader'
-        }, {
-            test: /\.(glsl|frag|vert)$/,
-            exclude: /node_modules/,
-            loader: 'raw-loader'
-        }, {
-            test: /\.(glsl|frag|vert)$/,
-            exclude: /node_modules/,
-            loader: 'glslify-loader'
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
         },
-        
-          {
-            test: /\.(glsl|frag|vert)$/,
-            exclude: /node_modules/,
-            loader: 'glslify-import-loader'
-        }, {
-            test: /\.(glsl|frag|vert)$/,
-            exclude: /node_modules/,
-            loader: 'raw-loader'
-        }, {
-            test: /\.(glsl|frag|vert)$/,
-            exclude: /node_modules/,
-            loader: 'glslify-loader'
-        },
-        {
-            test: /\.worker\.js$/,
-            use: [
-              { loader: 'worker-loader', options: { inline: 'no-fallback' } },
-            ],
-          },
-          
-        ],
-        
-        
-      },
-};
-    
+      ],
+    },
+  };
